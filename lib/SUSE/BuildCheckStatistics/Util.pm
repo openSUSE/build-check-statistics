bfb95636645234c6801895baf841367324697734
Sebastian Riedel <sri@cpan.org>
2016-11-22 16:03:29 +0100

    leave a little more space

--- lib/SUSE/BuildCheckStatistics/Util.pm
+++ lib/SUSE/BuildCheckStatistics/Util.pm
@@ -1,3 +1,4 @@
+#
 # Copyright (c) 2016 SUSE LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,6 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+#
 package SUSE::BuildCheckStatistics::Util;
 use Mojo::Base -strict;
 
